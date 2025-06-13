dfa9d53f742db857a4e91dc3cc752ac5bbf2e13e
Henry <95856301+henridough@users.noreply.github.com>
2025-06-13 01:56:33 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/henridough.github.io/', // 👈 Add this line
+  base: mode === 'production' ? '/henridough.github.io/' : '/',
   server: {
     host: "::",
     port: 8080,
